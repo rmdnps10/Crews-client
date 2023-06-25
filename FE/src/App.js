33aca19e43ff41acf41515b81e13dd2756cf817d
Goonco <s07019@naver.com>
2023-06-25 10:46:08 +0900

    :tada: Crews 화이팅

---
+++ FE/src/App.js
@@ -0,0 +1,14 @@
+import Router from "./Router";
+import GlobalStyle from "./style/GlobalStyle";
+import "./style/font.css";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
+}
+
+export default App;
---
+++ FE/src/Router.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
