9b7d98f86b85ac32f1ba253ec88604ee2b9e8883
koniiing <swanyjke@gmail.com>
2023-09-29 15:23:53 +0900

    :sparkles: 회원가입 페이지(#8)

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Navigation } from 'components/molecules';
 import { Hello, MakeForm } from 'pages';
+import { SignIn } from 'pages/SignIn';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Hello />} />
         <Route path="/makeform" element={<MakeForm />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </>
   );
