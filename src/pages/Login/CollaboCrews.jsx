e0893b619272694d32796b1ce188d019a7784ed4
leekyuho <rbgh0114@naver.com>
2023-09-30 23:10:21 +0900

    :sparkles: 아이디 저장 체크 시 쿠키에 저장, 재렌더시 자동입력 구현

--- src/pages/Login/CollaboCrews.jsx
+++ src/pages/Login/CollaboCrews.jsx
@@ -1,26 +1,22 @@
-import { Flex } from "components/atoms";
-import { useState } from "react";
-import styled from "styled-components";
-export const CollaboCrews =()=>{
-    const [clubList,setClubList] = useState([1,2,3]);
-    return (
-        <>
-            <Flex gap={"10"}>
-                {
-                    clubList.map(()=>{
-                        return(
-                            <CrewsCard/>
-                        );
-                    })
-                }
-            </Flex>
-        </>
-    );
-}
+import { Flex } from 'components/atoms';
+import { useState } from 'react';
+import styled from 'styled-components';
+export const CollaboCrews = () => {
+  const [clubList, setClubList] = useState([1, 2, 3]);
+  return (
+    <>
+      <Flex gap={'10'}>
+        {clubList.map(() => {
+          return <CrewsCard />;
+        })}
+      </Flex>
+    </>
+  );
+};
 
 const CrewsCard = styled.div`
-    width: 6.25rem;
-    height: 6.25rem;
-    border-radius : 100%;
-    background-color : black
+  width: 6.25rem;
+  height: 6.25rem;
+  border-radius: 100%;
+  background-color: black;
 `;
--- src/pages/Login/LoginInput.jsx
+++ src/pages/Login/LoginInput.jsx
@@ -1,61 +1,87 @@
-import { useState } from "react";
-import { Text } from "components/atoms";
-import { useCookies } from "react-cookie";
+import { useEffect, useState } from 'react';
+import { Flex, Text } from 'components/atoms';
+import { useCookies } from 'react-cookie';
 
-export const LoginInput = ()=>{
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [showPW, setShowPW] = useState(false);
-    //user id pw 쿠키
-    const [cookies, setCookie, removeCookie] = useCookies(["userCookie"]);
-    //id 저장할건지 체크박스 유무
-    const [storeId, setStoreId] = useState(false);
-    const toggleShowPW = (e)=>{
-        const isShowed = e.target.checked;
-        setShowPW(isShowed);
+export const LoginInput = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPW, setShowPW] = useState(false);
+  //user id pw 쿠키
+  const [cookies, setCookie, removeCookie] = useCookies([
+    'userEmail',
+    'userPassword',
+  ]);
+  //id 저장할건지 체크박스 유무
+  const [isStore, setIsStore] = useState(false);
+  useEffect(() => {
+    // 컴포넌트가 렌더링될 때 쿠키가 있으면 바로 입력
+    const savedEmail = cookies.userEmail;
+    const savedPassword = cookies.userPassword;
+
+    if (savedEmail && savedPassword) {
+      setEmail(savedEmail);
+      setPassword(savedPassword);
     }
-    const toggleStoreId = (e)=>{
-        const storeIdChecked = e.target.checked;
-        setStoreId(storeIdChecked);
+  }, []);
+  const toggleShowPW = (e) => {
+    const isShowed = e.target.checked;
+    setShowPW(isShowed);
+  };
+  const toggleIsStore = (e) => {
+    const isStoreChecked = e.target.checked;
+    setIsStore(isStoreChecked);
+  };
+  const onSumitHanlder = (e) => {
+    e.preventDefault();
+    if (email.trim() === '') {
+      alert('아이디를 입력하세요');
+    } else if (password.trim() === '') {
+      alert('비밀번호를 입력하세요');
+    } else {
+      //아이디 비번 input 초기화, 서버에서 있는지 확인하고 처리 여기서
+      setEmail('');
+      setPassword('');
+      if (isStore) {
+        const oneMonth = 30 * 24 * 60 * 60; // 1달 cookie 저장
+        setCookie('userEmail', email, { path: '/', maxAge: oneMonth });
+        setCookie('userPassword', password, { path: '/', maxAge: oneMonth });
+      }
     }
-    const onSumitHanlder=(e)=>{
-        e.preventDefault();
-        if(email.trim() === ""){
-            alert("아이디를 입력하세요");
-        }
-        else if(password.trim() === ""){
-            alert("비밀번호를 입력하세요");
-        }
-        else{//아이디 비번 input 초기화, api는 나중에
-            console.log(email);
-            console.log(password);
-            setEmail("");
-            setPassword("");
-        }
-    }
-    return(
-        <>
-            <form onSubmit={onSumitHanlder}>
-                <label>
-                    <Text>
-                        아이디
-                    </Text><br/>
-                    <input type='email' value={email} onChange={(e)=>(setEmail(e.target.value))}/>
-                </label>
-                <br/>      
-                <label>
-                    <Text>
-                        비밀번호
-                    </Text><br/>
-                    <input type={showPW? "text":"password"} value={password} onChange={(e)=>(setPassword(e.target.value))}/>
-                </label>
-                <br/>
-                <button type="submit"><Text>로그인</Text></button>
-            </form>
-            <input type="checkbox" checked={storeId} onChange={toggleStoreId}/>
-            <Text>아이디 저장</Text>
-            <input type="checkbox" checked={showPW} onChange={toggleShowPW}></input>
-            <Text>비밀번호 표시</Text>
-        </>
-    );
-}
+  };
+  const storeCookie = () => {};
+  return (
+    <>
+      <form onSubmit={onSumitHanlder}>
+        <label>
+          <Text>아이디</Text>
+          <br />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          <Text>비밀번호</Text>
+          <br />
+          <input
+            type={showPW ? 'text' : 'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">
+          <Text>로그인</Text>
+        </button>
+      </form>
+      <Flex>
+        <input type="checkbox" checked={isStore} onChange={toggleIsStore} />
+        <Text>아이디 저장</Text>
+        <input type="checkbox" checked={showPW} onChange={toggleShowPW}></input>
+        <Text>비밀번호 표시</Text>
+      </Flex>
+    </>
+  );
+};
