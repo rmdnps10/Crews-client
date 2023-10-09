e5f26025ba569f27501ddfc849a6246714b608c8
Goonco <s07019@naver.com>
2023-10-09 13:40:07 +0900

    :lipstick: Brand Color updates (#1)

---
+++ src/style/palette.js
@@ -0,0 +1,25 @@
+/* -------------- Brand Black -------------- */
+export const BK01 = '#303030';
+export const BK02 = '#101010';
+
+/* -------------- Brand Gray -------------- */
+export const G01 = '#F2F2F2';
+export const GO2 = '#E6E6E6';
+export const GO3 = '#CCCCCC';
+export const GO4 = '#B3B3B3';
+export const GO5 = '#999999';
+export const GO6 = '#666666';
+
+/* -------------- Brand Blue -------------- */
+export const B01 = '#F6F9FE';
+export const B02 = '#E8EFFD';
+export const B03 = '#BACFF8';
+export const B04 = '#5D90EE';
+export const B05 = '#3172EA';
+export const B06 = '#1557D0';
+export const B07 = '#0C3074';
+
+/* -------------- Brand Red -------------- */
+export const R01 = '#FBEAEA';
+export const R02 = '#DB4242';
+export const R03 = '#FB3D3D';
