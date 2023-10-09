6ba74106ad96611710bce17d9c522f24ae479778
Goonco <s07019@naver.com>
2023-10-09 15:05:09 +0900

    :fire: test page deleted (#1)

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import { Navigation } from 'components/molecules';
 
-import { Hello, MakeForm, MakePost, Login ,SignIn} from 'pages';
-
+import { MakeForm, MakePost, Login, SignIn } from 'pages';
 
 const Router = () => {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Hello />} />
         <Route path="/makeform" element={<MakeForm />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/makepost" element={<MakePost/>}/>
+        <Route path="/makepost" element={<MakePost />} />
         <Route path="/login" element={<Login />} />
-
       </Routes>
     </>
   );
