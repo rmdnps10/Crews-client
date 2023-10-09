d9795e6b9e7ddff022eaf2d4c9721caf3c1f5b55
Goonco <s07019@naver.com>
2023-10-09 15:58:58 +0900

    :lipstick: Add Brand White to palette (#1)

--- src/style/palette.js
+++ src/style/palette.js
@@ -4,11 +4,11 @@ export const BK02 = '#101010';
 
 /* -------------- Brand Gray -------------- */
 export const G01 = '#F2F2F2';
-export const GO2 = '#E6E6E6';
-export const GO3 = '#CCCCCC';
-export const GO4 = '#B3B3B3';
-export const GO5 = '#999999';
-export const GO6 = '#666666';
+export const G02 = '#E6E6E6';
+export const G03 = '#CCCCCC';
+export const G04 = '#B3B3B3';
+export const G05 = '#999999';
+export const G06 = '#666666';
 
 /* -------------- Brand Blue -------------- */
 export const B01 = '#F6F9FE';
@@ -23,3 +23,6 @@ export const B07 = '#0C3074';
 export const R01 = '#FBEAEA';
 export const R02 = '#DB4242';
 export const R03 = '#FB3D3D';
+
+/* -------------- Brand White -------------- */
+export const W01 = '#FFFFFF';
