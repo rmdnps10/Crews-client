afe48b6be9f52316f87a57b03367771beb8d0fbe
leekyuho <rbgh0114@naver.com>
2023-10-11 13:51:43 +0900

    :sparkles: 아이디, 비밀번호 focus 시 status 변경 구현 (#5)

--- src/pages/Login/LoginInput.jsx
+++ src/pages/Login/LoginInput.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
-import { Flex, Text } from 'components/atoms';
 import { useCookies } from 'react-cookie';
+import { BK02 } from 'style/palette';
+//imported components
+import { Flex, Input, Text } from 'components/atoms';
 
 export const LoginInput = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  //input box status
+  const [emailStatus, setEmailStatus] = useState('inactive');
+  const [passwordStatus, setPasswordStatus] = useState('inactive');
+  //비밀번호 표시 toggle
   const [showPW, setShowPW] = useState(false);
   //user id pw 쿠키
   const [cookies, setCookie, removeCookie] = useCookies([
@@ -53,22 +59,42 @@ export const LoginInput = () => {
     <>
       <form onSubmit={onSumitHanlder}>
         <label>
-          <Text>아이디</Text>
+          <Text children="아이디" color={BK02} />
           <br />
-          <input
+          <Input
+            status={emailStatus}
+            placeholder="이메일을 입력해주세요."
+            width="32.625rem"
+            height="4.25rem"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            onFocus={() => {
+              setEmailStatus('active');
+            }}
+            onBlur={() => {
+              setEmailStatus('inactive');
+            }}
           />
         </label>
         <br />
         <label>
-          <Text>비밀번호</Text>
+          <Text children="비밀번호" color={BK02} />
           <br />
-          <input
+          <Input
+            status={passwordStatus}
+            placeholder="비밀번호를 입력해주세요."
+            width="32.625rem"
+            height="4.25rem"
             type={showPW ? 'text' : 'password'}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            onFocus={() => {
+              setPasswordStatus('active');
+            }}
+            onBlur={() => {
+              setPasswordStatus('inactive');
+            }}
           />
         </label>
         <br />
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,31 +1,27 @@
 import { Flex, Space, Text } from 'components/atoms';
-import { CollaboCrews } from './CollaboCrews';
+// import { CollaboCrews } from './CollaboCrews';
 import { LoginInput } from './LoginInput';
+import { BK01 } from '../../style/palette';
 export const Login = () => {
   return (
     <div>
       <Flex direction="column">
-        <Text>
-          동아리 리크루팅과 지원을 한번에,
-          <br />
-        </Text>
-        <Text color="#3172EA"> Crews🚢</Text>
-        <LoginInput /*더 나은 방법 없나...*/ />
+        <Text children="동아리 리크루팅과 지원을 한번에," />
+        <Text children="Crews🚢" color="#3172EA" />
+        <LoginInput />
         <Flex>
-          <Text>아직 회원이 아니신가요?</Text>
           <Text
-            color="#3172EA"
+            children="회원가입"
+            color={BK01}
             cursor="pointer"
             onClick={() => {
               alert('아직 미구현, 회원가입으로 navigate');
             }}
-          >
-            회원가입
-          </Text>
+          />
         </Flex>
         <Space height={'2rem'} />
-        <Text>Crews와 함께하는 동아리</Text>
-        <CollaboCrews />
+
+        {/* <CollaboCrews /> */}
       </Flex>
     </div>
   );
