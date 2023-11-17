7166a4824e8f0fef6b6714e334f6181f8514ded7
inyoung <rmdnps10@gmail.com>
2023-11-17 15:41:41 +0900

    :sparkles: 공고 선택 여부에 따른 view 구현 #22

--- src/pages/MyPage/MyPostSection.js
+++ src/pages/MyPage/MyPostSection.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useState } from 'react';
 function MyPostSection() {
-  return <MyPostSectionWrapper></MyPostSectionWrapper>;
+  const [isApplyPost, setIsApplyPost] = useState(true);
+  const showApplyPost = () => {
+    setIsApplyPost(true);
+  };
+  const showSavePost = () => {
+    setIsApplyPost(false);
+  };
+  return (
+    <MyPostSectionWrapper>
+      <SelectPostType>
+        <ApplyPost $isBlue={isApplyPost} onClick={showApplyPost}>
+          지원한 공고
+        </ApplyPost>
+        <SavePost $isBlue={!isApplyPost} onClick={showSavePost}>
+          찜한 공고
+        </SavePost>
+        <PostList></PostList>
+      </SelectPostType>
+    </MyPostSectionWrapper>
+  );
 }
 
 const MyPostSectionWrapper = styled.section`
-
+  margin-top: 81px;
 `;
+
+const SelectPostType = styled.div`
+  display: flex;
+  gap: 32px;
+`;
+const ApplyPost = styled.div`
+  color: ${(props) => (props.$isBlue ? '#1557d0' : '#B3B3B3')};
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -0.4px;
+  border-bottom: ${(props) => (props.$isBlue ? '4px solid #1557d0' : 'none')};
+`;
+const SavePost = styled(ApplyPost)``;
+
+const PostList = styled.div``;
 
 export default MyPostSection;
--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import MyProfileSection from './MyProfileSection';
+import MyPostSection from './MyPostSection';
 export const MyPage = () => {
   return (
     <MyPageContainer>
       <MyProfileSection />
+      <MyPostSection />
     </MyPageContainer>
   );
 };
