68abefbfd2e1a87f65480118df9a8f0a0d48c8a4
Goonco <s07019@naver.com>
2023-11-22 03:12:35 +0900

    :sparkles: Commit Before Merge

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import { Navigation } from 'components/molecules';
+import { Header } from 'components/molecules';
 
 import { MakeForm, MakePost, Login, SignIn } from 'pages';
 
 const Router = () => {
   return (
     <>
-      <Navigation />
+      <Header />
       <Routes>
         <Route path="/makeform" element={<MakeForm />} />
         <Route path="/signin" element={<SignIn />} />
