727bada8e5ca143cd0db1f25a89ab4cc9cb9fbb3
koniiing <swanyjke@gmail.com>
2023-11-22 18:29:58 +0900

    병합을 위한 커밋

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,7 @@ export * from './MakeForm';
 export * from './MakePost';
 export * from './Login';
 export * from './SignIn';
-<<<<<<< HEAD
 export * from './PostDetail';
-=======
 export * from './Evaluate';
 export * from './EvaluteDetail';
 export * from './MyPage';
->>>>>>> 7df8c8b5c0e646382bbed2ef555bb456550f2b09
