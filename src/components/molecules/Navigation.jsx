5a9765247db55bd3f32659e9e1203f5184ca4034
inyoung <rmdnps10@gmail.com>
2023-11-22 19:38:56 +0900

    :lipstick: 내비게이션 스타일링 완료

--- src/components/molecules/Navigation.jsx
+++ src/components/molecules/Navigation.jsx
@@ -1,20 +1,35 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-
+import { Flex } from 'components/atoms';
+import basicProfile from './basic-profile.svg';
 export const Navigation = () => {
   const nav = useNavigate();
 
   return (
-    <NavigationContainer>
-      <button onClick={() => nav('/')}>To Home</button>
-      <button onClick={() => nav('/makeform')}>To MakeForm</button>
-      <button onClick={() => nav('/login')}>To Login</button>
-      <button onClick={() => nav('/makepost')}>To MakePost</button>
-      <button onClick={() => nav('/signin')}>To SignIn</button>
-      <button onClick={() => nav('/evaluate')}>To Evaluate</button>
-      <button onClick={() => nav('/evaluatedetail')}>To EvaluateDetail</button>
-      <button onClick={() => nav('/mypage')}>mypage</button>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <button onClick={() => nav('/')}>To Home</button>
+        <button onClick={() => nav('/makeform')}>To MakeForm</button>
+        <button onClick={() => nav('/login')}>To Login</button>
+        <button onClick={() => nav('/makepost')}>To MakePost</button>
+        <button onClick={() => nav('/signin')}>To SignIn</button>
+        <button onClick={() => nav('/evaluate')}>To Evaluate</button>
+        <button onClick={() => nav('/evaluatedetail')}>
+          To EvaluateDetail
+        </button>
+        <button onClick={() => nav('/mypage')}>mypage</button>
+      </NavigationContainer>
+      <CrewsNav>
+        <CrewsLogo>Crews 🛳️</CrewsLogo>
+        <ProfileSection>
+          <ProfileImage src={basicProfile} />
+          <Flex direction="column" align="flex-start">
+            <UserName>C123456 김서강</UserName>
+            <LoginButton>로그아웃</LoginButton>
+          </Flex>
+        </ProfileSection>
+      </CrewsNav>
+    </>
   );
 };
 
@@ -23,4 +38,51 @@ const NavigationContainer = styled.div`
   flex-direction: row;
   background-color: yellow;
   gap: 20px;
+`;
+
+const CrewsNav = styled.div`
+  display: flex;
+  align-items: center;
+  height: 80px;
+  background: var(--blue-b-02, #e8effd);
+`;
+
+const CrewsLogo = styled.div`
+  margin-left: 25px;
+  color: var(--blue-b-05-m, #3172ea);
+  font-family: Poppin;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -0.6px;
+`;
+
+const ProfileSection = styled.div`
+  margin-left: auto;
+  margin-right: 28px;
+  display: flex;
+  gap: 12px;
+`;
+
+const ProfileImage = styled.img``;
+
+const UserName = styled.div`
+  color: var(--black-bk-01, #303030);
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: -0.36px;
+`;
+const LoginButton = styled.div`
+  color: var(--black-bk-01, #303030);
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.28px;
+  text-decoration-line: underline;
 `;
